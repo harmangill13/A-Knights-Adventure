437bf65e54a015a882834fad9b7e86533b5ad003
HarmanGill <harman97gill@gmail.com>
2024-02-29 00:00:01 -0500

    started css, peuso code js

---
+++ js/app.js
@@ -0,0 +1,12 @@
+// create variable that will be outside our function and need to be called on whenever
+
+
+// Create the onload function
+
+
+// create the game loop to set to make the game turn based
+
+
+// make the functions and attach them to the buttons to allow players to perform functions within the game loop
+
+
