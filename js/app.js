0a2381d06799115f35e650c8ece2c3c48368b137
HarmanGill <harman97gill@gmail.com>
2024-02-29 19:06:23 -0500

    pseudo coding functions, items, players

--- js/app.js
+++ js/app.js
@@ -1,4 +1,50 @@
-// create variable that will be outside our function and need to be called on whenever
+// create variables that will be outside our function and need to be called on whenever
+const playerOne = {
+  hitpoints: 100,
+  currentWeapon: {
+    name: "Bronze knife",
+    accuarcy: 1,
+    damage: 5,
+  },
+  Gold: 50,
+}
+
+const playerTwo = {
+  hitpoints: 100,
+  currentWeapon: {
+    name: "Bronze knife",
+    accuarcy: 1,
+    damage: 5,
+  },
+  Gold: 50,
+}
+
+const items = [
+  {
+    name: "Steel sword",
+    accuarcy: .80,
+    damage: 10,
+    cost: 100,
+  },
+  {
+    name: "Rune 2h",
+    accuarcy: .50,
+    damage: 40,
+    cost: 500,
+  },
+  {
+    name: "Magic bow",
+    accuarcy: 1,
+    damage: 10,
+    cost: 200
+  },
+  {
+    name: "Fire staff",
+    accuarcy: .80,
+    damage: 30,
+    cost: 700,
+  }
+]
 
 
 // Create the onload function
@@ -6,6 +52,13 @@
 
 // create the game loop to set to make the game turn based
 
+// Game loop consists of functions that will allow the players to choose how to spend their time before facing off against the final boss.
+// FUNCTIONS:
+  // Shop ()
+      // to buy a different weapon
+  // Kill Goblin ()
+      // To get gold coins 
+  // 
 
 // make the functions and attach them to the buttons to allow players to perform functions within the game loop
 
